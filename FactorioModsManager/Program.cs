ebfafea8e79f5a9062b2284be8f850f2b55337fc
Jan Mischak <jan.mischak@kstp.at>
2020-10-22 03:39:36 +0200

    Initial commit
    Add solution
    Add FactorioModsManager
    Add FactorioModPortalClient submodule

---
+++ FactorioModsManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactorioModsManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
