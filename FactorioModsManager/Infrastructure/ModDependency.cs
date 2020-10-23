aa38a32178bffeb8d46ffa0f24508a0189295c7e
Jan Mischak <jan.mischak@kstp.at>
2020-10-23 15:32:16 +0200

    Add supoort for unresolved ModDependency s

--- FactorioModsManager/Infrastructure/ModDependency.cs
+++ FactorioModsManager/Infrastructure/ModDependency.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 
 namespace FactorioModsManager.Infrastructure
 {
@@ -12,11 +13,34 @@ namespace FactorioModsManager.Infrastructure
             DependencyType = dependencyType;
         }
 
+        public ModDependency(ReleaseData sourceRelease, string targetModName, ModDependencyType dependencyType)
+        {
+            SourceRelease = sourceRelease;
+            TargetModName = targetModName;
+            DependencyType = dependencyType;
+        }
+
         [DataMember(/*IsRequired = true*/)]
         public ReleaseData SourceRelease { get; set; }
 
-        [DataMember(/*IsRequired = true*/)]
-        public ModData TargetMod { get; set; }
+        [DataMember(IsRequired = false)]
+        public ModData? TargetMod { get; set; }
+
+        [DataMember(IsRequired = false)]
+        public string? TargetModName { get; set; }
+
+        public string GetTargetModName()
+        {
+            if (TargetModName != null
+                && TargetMod != null
+                && TargetModName != TargetMod.Name)
+                throw new Exception($"{nameof(TargetModName)} ({TargetModName}) does not match " +
+                    $"{nameof(TargetMod)}.{nameof(TargetMod.Name)} ({TargetMod.Name}).");
+
+            return TargetModName ?? TargetMod?.Name
+                ?? throw new Exception($"Unable to get the target mod name of a {nameof(ModDependency)} " +
+                $"for {nameof(TargetMod)} and {nameof(TargetMod)}.{nameof(TargetMod.Name)} are both null.");
+        }
 
         [DataMember(/*IsRequired = true*/)]
         public ModDependencyType DependencyType { get; set; }
