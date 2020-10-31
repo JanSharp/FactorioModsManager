453528d1ceb79aabc941b0a6de1de5bb45084221
Jan Mischak <jan.mischak@kstp.at>
2020-10-31 19:54:46 +0100

    Remove and sort usings

--- FactorioModsManager/Services/IConfigService.cs
+++ FactorioModsManager/Services/IConfigService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using FactorioModsManager.Infrastructure;
+﻿using FactorioModsManager.Infrastructure;
 
 namespace FactorioModsManager.Services
 {
